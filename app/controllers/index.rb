367a93d89b84bac950802ec89a61a065a02e4dff
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-08 16:45:37 -0500

    Recent Tweets 2

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -4,8 +4,17 @@ end
 
 get '/:username' do
   @user = TwitterUser.find_or_create_by(:screen_name => params[:username])
-  @user.fetch_tweets! if @user.tweets_stale?
+  if @user.tweets_stale?
+  @user.fetch_tweets!
+  end
   @tweets = @user.tweets.limit(10)
   p @tweets.first
   erb :index
+end
+
+post '/get_tweets' do
+  user = TwitterUser.find(params[:user_id])
+  if user.tweets_stale?
+    user.fetch_tweets!
+  end
 end
